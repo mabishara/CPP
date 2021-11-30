3e65c879cf065f1a5824a59309a4d98b656c0bd4
mabishara <58200389+mabishara@users.noreply.github.com>
2021-11-29 23:12:42 -0800

    Add files via upload

---
+++ lab-5-asymmetric-fifo-buffer-kylehanano_michaelbishara-master/Lab_5_MB_KH/sources_1/new/fifo.sv
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/29/2020 05:20:32 PM
+// Design Name: 
+// Module Name: fifo
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module fifo
+    #(parameter ADDR_WIDTH = 3, DATA_WIDTH = 8)
+    (
+        input logic clk, reset,
+        input logic wr, rd,
+        input logic [(DATA_WIDTH*2) - 1: 0] w_data,
+        output logic [DATA_WIDTH - 1: 0] r_data,
+        output logic full, empty
+    );
+    
+    // signal declaration
+    logic r_count;
+    logic [ADDR_WIDTH - 1: 0] w_addr, r_addr;
+    
+    // instantiate register file
+    reg_file #(.ADDR_WIDTH(ADDR_WIDTH), .DATA_WIDTH(DATA_WIDTH))
+        r_file_unit (.w_en(wr & ~full), .*);
+
+    // instantiate fifo controller
+    fifo_ctrl #(.ADDR_WIDTH(ADDR_WIDTH))
+        ctrl_unit (.*);                    
+endmodule
